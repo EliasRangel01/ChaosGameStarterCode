57620d3613773babb51ffb33c0ba9ea771e82194
W1811636 <w1811636@apps.losrios.edu>
2023-04-19 11:25:30 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,63 +1,123 @@
 // Include important C++ libraries here
-#include <sstream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
-
+#include <iostream>
+#include <sstream>
+//test
 // Make code easier to type with "using namespace"
 using namespace sf;
 using namespace std;
-
 int main()
 {
-	// for linux fontsm ./font/fontname
-	View view(FloatRect(0.f, 0.f, 1920.f, 1080.f));
-	
+	float width = sf::VideoMode::getDesktopMode().width;
+	float height = sf::VideoMode::getDesktopMode().height;
+	sf::View view(sf::FloatRect(0.f, 0.f, width, height));
 	// Create a video mode object
-	VideoMode vm(800, 600);
-
+	VideoMode vm(width, height);
+	//VideoMode vm(800, 600);
 	// Create and open a window for the game
-	RenderWindow window(vm, "Chaos!", Style::Default);
-	window.setView(view);
-
+	RenderWindow window(vm, "Chaos Game!", Style::Default);
 	vector<Vector2f> vertices;
+	vector<Vector2f> points;
 
-	//copy code from timber and delete what dont need
+	Text messageText;
+	// We need to choose a font
+	Font font;
+	font.loadFromFile("./fonts/Roboto-Black.ttf");
+	messageText.setFont(font);
+	messageText.setCharacterSize(50);
+
 	while (window.isOpen())
 	{
+		/*
+		****************************************
+		Handle the players input
+		****************************************
+		*/
 		Event event;
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::Closed)
-			{
+			if (event.type == sf::Event::Closed)
 				window.close();
-
-			}
-			if (event.mouseButton.button == Mouse::Left)
+			if (event.type == sf::Event::MouseButtonPressed)
 			{
-				Vector2i pixelPos = { event.mouseButton.x , event.mouseButton.y };
-				Vector2f worldPos = window.mapPixelToCoords(pixelPos, view);
-				vertices.push_back(worldPos);
+				if (event.mouseButton.button == sf::Mouse::Left && vertices.size() < 3)
+				{
+
+					Vector2i pixelPos = { event.mouseButton.x, event.mouseButton.y };
+					Vector2f worldPos = window.mapPixelToCoords(pixelPos, view);
+					vertices.push_back(worldPos);
+
+					//vertices.push_back(Vector2f((float)event.mouseButton.x,
+					//	(float)event.mouseButton.y));
+				}
+				if (event.mouseButton.button == sf::Mouse::Left && points.size() > 0)
+				{
+					for (size_t i = 0; i < 100; i++)
+					{
+
+						Vector2i pixelPos = { event.mouseButton.x, event.mouseButton.y };
+						Vector2f worldPos = window.mapPixelToCoords(pixelPos, view);
+						points.push_back(worldPos);
+					}
+
+				}
+
+
 			}
-
 		}
-
-
 		if (Keyboard::isKeyPressed(Keyboard::Escape))
 		{
 			window.close();
 		}
-		RectangleShape shape{ Vector2f(4,4) };
+		/*
+		****************************************
+		Update the scene
+		****************************************
+		*/
+		// Measure time
+		//Time dt = clock.restart();
 
+		// Update the text
+		std::stringstream ss;
+		ss << "Vertex count = " << vertices.size();
+		messageText.setString(ss.str());
+		FloatRect textRect = messageText.getLocalBounds();
+		messageText.setOrigin(textRect.left +
+			textRect.width / 2.0f,
+			textRect.top +
+			textRect.height / 2.0f);
+		messageText.setPosition(width / 2.0f, 100);
 
+		/*
+		****************************************
+		Draw the scene
+		****************************************
+		*/
+		window.setView(view);
+		// Clear everything from the last run frame
+		window.clear();
+		// Draw our game scene here
 
+		RectangleShape shape{ Vector2f{4,4} };
+		shape.setFillColor(Color::Cyan);
 		for (size_t i = 0; i < vertices.size(); i++)
 		{
 			shape.setPosition(vertices.at(i));
 			window.draw(shape);
 		}
 
-
-
+		RectangleShape shape2{ Vector2f{4,4} };
+		shape.setFillColor(Color::Red);
+		for (size_t i = 0; i < points.size(); i++)
+		{
+			shape2.setPosition(points.at(i));
+			window.draw(shape);
+		}
+		window.draw(messageText);
+		// Show everything we just drew
+		window.display();
 	}
-	
+
+	return 0;
 }
